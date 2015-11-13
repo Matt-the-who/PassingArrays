c0451aaa3b33d6878f9908fdb1e55377c7cf52df
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-11-13 14:25:44 -0500

    Largest Number in an array

---
+++ PassingArrays/PassingArrays.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PassingArrays
+{
+    partial class frmPassingArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstAListbox = new System.Windows.Forms.ListBox();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(94, 38);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(56, 208);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 1;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 2;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            // 
+            // lstAListbox
+            // 
+            this.lstAListbox.FormattingEnabled = true;
+            this.lstAListbox.Location = new System.Drawing.Point(68, 67);
+            this.lstAListbox.Name = "lstAListbox";
+            this.lstAListbox.Size = new System.Drawing.Size(120, 95);
+            this.lstAListbox.TabIndex = 3;
+            // 
+            // frmPassingArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstAListbox);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmPassingArrays";
+            this.Text = "Passing Arrays";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ListBox lstAListbox;
+    }
+}
+
---
+++ PassingArrays/PassingArrays.cs
@@ -0,0 +1,69 @@
+﻿/*
+Created by Matthew Ho
+Created on 11-13-15
+Created for ICS3U
+Assignment #5-02
+This program passes an array into a function.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassingArrays
+{
+    public partial class frmPassingArrays : Form
+    {
+        Random rnd = new Random();
+
+        public int ArrayCheck(int [] theArray)
+        {//Finds the largest # in array
+            int largestNumber = 0;
+
+            foreach (int aSingleInteger in theArray)
+            {
+                if (aSingleInteger > largestNumber)
+                {
+                    largestNumber = aSingleInteger;
+                }
+
+            } 
+
+
+            return largestNumber;
+        }
+
+        public frmPassingArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //variable
+            int[] arrayOfNumbers = new int[5];
+            int numbersGenerated = 0;
+
+            //Clearing array and listbox
+            this.lstAListbox.Items.Clear();
+            Array.Clear(arrayOfNumbers, 0, 5);
+
+            //for loop to assign each spot in the a with a random #
+            for (int counter = 0; counter < 5; counter++)
+            {
+                numbersGenerated = rnd.Next(1, 10+1);
+                arrayOfNumbers[counter] = numbersGenerated;
+                this.lstAListbox.Items.Add(numbersGenerated);
+            }
+
+            //Output
+            this.lblAnswer.Text = "The largest number is " + ArrayCheck(arrayOfNumbers);
+        }
+    }
+}
